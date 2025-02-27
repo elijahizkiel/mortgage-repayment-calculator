32e189ded8a0214ff2414de9d1d8fa7857abddaa
=elijahizkiel <=>
2025-02-27 14:38:02 +0300

    import assests to be used in app

---
+++ src/assets/assets.js
@@ -0,0 +1,9 @@
+import calcIcon from "./images/icon-calculator.svg";
+import illustrationEmpty from "./images/illustration-empty.svg"
+
+const assets ={
+    calcIcon:calcIcon,
+    illustrationEmpty: illustrationEmpty,
+}
+
+export default assets;
